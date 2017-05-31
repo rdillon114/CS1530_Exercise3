fb6db07c674720ca512fa99893ea257b1c2117ab
Dillon <dillonminerr@gmail.com>
2017-05-31 18:28:16 -0400

    syu

--- src/test/java/schmeckle/CS1530_EX3_TEST.java
+++ src/test/java/schmeckle/CS1530_EX3_TEST.java
@@ -1,7 +1,7 @@
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-import newpackage.CS1530_EX3;
+import package.schmeckle;
 
 /**
  *
@@ -12,27 +12,27 @@ public class CS1530_EX3_TEST {
     public void testLazy4() {
         assertEquals(CS1530_EX3.Lazy(4), 11);
     }
-    
+
     @Test
     public void testLazy0() {
         assertEquals(CS1530_EX3.Lazy(0), 1);
     }
-    
+
     @Test
     public void testLazy2() {
         assertEquals(CS1530_EX3.Lazy(2), 4);
     }
-    
+
     @Test
     public void testTri0() {
         assertEquals(CS1530_EX3.Tri(0), 0);
     }
-    
+
     @Test
     public void testTri5() {
         assertEquals(CS1530_EX3.Tri(5), 15);
     }
-    
+
     @Test
     public void testTri9() {
         assertEquals(CS1530_EX3.Tri(9), 45);
